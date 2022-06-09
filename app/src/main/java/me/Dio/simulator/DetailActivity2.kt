548abc2edafe3d178b2ce0715007f6ecf3b2f46e
Perles <luanc.mendes@hotmail.com>
2022-06-09 19:59:03 -0300

    Desenvolvimento dos layouts de listagem e detalhes das partidas.

---
+++ app/src/main/java/me/Dio/simulator/DetailActivity2.kt
@@ -0,0 +1,18 @@
+package me.Dio.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.Dio.simulator.databinding.ActivityDetail2Binding
+
+class DetailActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityDetail2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetail2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
